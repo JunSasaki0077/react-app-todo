c8b5a207be6673dd2e736139592d71045f708890
Jun Sasaki <71161710+JunSasaki0077@users.noreply.github.com>
2022-04-05 08:09:39 +0900

    cssを追加

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,29 @@ import "./styles.css";
 export const App = () => {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input type="text" placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のTODO</p>
+      <div className="incomplete-area">
+        <p className="title"> 未完了のTODO</p>
         <ul>
-          <li>ああああ</li>
-          <button>完了</button>
-          <button> 削除</button>
+          <div className="list-row">
+            <li>ああああ</li>
+            <button>完了</button>
+            <button> 削除</button>
+          </div>
         </ul>
       </div>
-      <div></div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          <div className="list-row">
+            <li>aaaa</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
     </>
   );
 };
