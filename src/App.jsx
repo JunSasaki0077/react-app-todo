c0f6e77a4c9dfa7cebd93ad1a64335977920365d
Jun Sasaki <71161710+JunSasaki0077@users.noreply.github.com>
2022-04-05 07:48:55 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+export const App = () => {
+  return (
+    <>
+      <div>
+        <input type="text" placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>ああああ</li>
+          <button>完了</button>
+          <button> 削除</button>
+        </ul>
+      </div>
+      <div></div>
+    </>
+  );
+};
