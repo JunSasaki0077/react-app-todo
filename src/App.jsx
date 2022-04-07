1bcfff7502331eabcf9665cbb1d9dc544454490e
Jun Sasaki <71161710+JunSasaki0077@users.noreply.github.com>
2022-04-08 07:30:41 +0900

    追加ボタンでtodoリストに追加機能

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,29 @@ import { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState("");
   const [incomplateTodos, setIncomplateTodos] = useState([
     "ああああ",
     "いいいい"
   ]);
   const [complateTodos, setComplateTodos] = useState(["うううう"]);
+  const onChangeTodoText = (e) => setTodoText(e.target.value);
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incomplateTodos, todoText];
+    setIncomplateTodos(newTodos);
+    setTodoText("");
+  };
   return (
     <>
       <div className="input-area">
-        <input type="text" placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          type="text"
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title"> 未完了のTODO</p>
